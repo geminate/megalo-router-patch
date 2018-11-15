a2a3e2b6ea4ae231d9200fc9782cf4b555661e15
liuhuihao <1061036802@qq.com>
2018-11-15 16:04:45 +0800

    feat: change methods for swan and alipay

--- src/utils/methods.js
+++ src/utils/methods.js
@@ -1,31 +1,45 @@
 import {parseUrl} from './query';
 
-const go = (delta) => {
-    wx.navigateBack({delta});
+const go = (delta, platform = 'wechat') => {
+    (platform == 'wechat') && wx.navigateBack({delta});
+    (platform == 'swan') && swan.navigateBack({delta});
+    (platform == 'alipay') && my.navigateBack({delta});
 };
 
-const back = () => {
-    wx.navigateBack();
+const back = (platform = 'wechat') => {
+    (platform == 'wechat') && wx.navigateBack();
+    (platform == 'swan') && swan.navigateBack();
+    (platform == 'alipay') && my.navigateBack();
 };
 
-const replace = (location, complete, fail, success) => {
+const replace = (location, complete, fail, success, platform = 'wechat') => {
     const url = parseUrl(location);
-    wx.redirectTo({url, complete, fail, success});
+    (platform == 'wechat') && wx.redirectTo({url, complete, fail, success});
+    (platform == 'swan') && swan.redirectTo({url, complete, fail, success});
+    (platform == 'alipay') && my.redirectTo({url, complete, fail, success});
 };
 
-const push = (location, complete, fail, success) => {
+const push = (location, complete, fail, success, platform = 'wechat') => {
     const url = parseUrl(location);
     const params = {url, complete, fail, success};
 
     if (location.isTab) {
-        wx.switchTab(params);
+        (platform == 'wechat') && wx.switchTab(params);
+        (platform == 'swan') && swan.switchTab(params);
+        (platform == 'alipay') && my.switchTab(params);
+
         return;
     }
     if (location.reLaunch) {
-        wx.reLaunch(params);
+        (platform == 'wechat') && wx.reLaunch(params);
+        (platform == 'swan') && swan.reLaunch(params);
+        (platform == 'alipay') && my.reLaunch(params);
         return;
     }
-    wx.navigateTo(params);
+    (platform == 'wechat') && wx.navigateTo(params);
+    (platform == 'swan') && swan.navigateTo(params);
+    (platform == 'alipay') && my.navigateTo(params);
+
 };
 
 export {go, back, push, replace};
